311de23c941b8e6ff7992c7281c1d50fea95e3f7
Al-rimi <ggak71@gmail.com>
2025-03-12 21:01:46 +0800

    Firefox support

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -3,7 +3,7 @@ import { deploy } from '../utils/deploy';
 
 export function deployTomcat(): void {
     const subAction = vscode.window.showQuickPick(['Fast', 'Maven', 'Gradle'], {
-        placeHolder: 'Select deployment type'
+        placeHolder: 'Select build type'
     });
     subAction.then((type) => {
         if (type === 'Fast' || type === 'Maven' || type === 'Gradle') {
--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -4,29 +4,31 @@ import WebSocket from 'ws';
 import * as vscode from 'vscode';
 import { defaultStatusBar, updateStatusBar } from '../extension';
 
-function getBrowserCommand(browser: string): string {
+function getBrowserCommand(browser: string): string | null{
     switch (browser) {
-        // case 'Firefox': // Not supported.
-        //     return process.platform === 'win32' ? 'start firefox' : 'firefox';
+        case 'Firefox':
+            return process.platform === 'win32' ? 'start firefox' : process.platform === 'darwin' ? '/Applications/Firefox.app/Contents/MacOS/firefox' : 'firefox';
         case 'Microsoft Edge':
-            return process.platform === 'win32' ? 'start msedge' : 'msedge';
-        case 'Safari':
-            return process.platform === 'darwin' ? 'open -a "Safari"' : '';
+            return process.platform === 'win32' ? 'start msedge' : process.platform === 'darwin' ? '/Applications/Microsoft\ Edge.app/Contents/MacOS/Microsoft\ Edge' : 'msedge';
         case 'Brave':
-            return process.platform === 'win32' ? 'start brave' : process.platform === 'darwin' ? 'open -a "Brave Browser"' : 'brave-browser';
+            return process.platform === 'win32' ? 'start brave' : process.platform === 'darwin' ? '/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser' : 'brave-browser';
         case 'Opera':
-            return process.platform === 'win32' ? 'start opera' : process.platform === 'darwin' ? 'open -a "Opera"' : 'opera';
+            return process.platform === 'win32' ? 'start opera' : process.platform === 'darwin' ? '/Applications/Opera.app/Contents/MacOS/Opera' : 'opera';
+        case 'Safari':
+            return process.platform === 'darwin' ? 'open -a "Safari"' : null;
         default:
-            return process.platform === 'win32' ? 'start chrome' : process.platform === 'darwin' ? 'open -a "Google Chrome"' : 'google-chrome';
+            return process.platform === 'win32' ? 'start chrome' : process.platform === 'darwin' ? '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome' : 'google-chrome';
     }
 }
 
 export async function runBrowser(appName: string): Promise<void> {
     const browser = vscode.workspace.getConfiguration('tomcat').get<string>('defaultBrowser') || 'chrome';
-    const targetPort = 9222;
+    const debugPort = 9222;
     const appUrl = `http://localhost:${vscode.workspace.getConfiguration().get('tomcat.port', 8080)}/${appName}`;
-    const debugUrl = `http://localhost:${targetPort}/json`;
+    const debugUrl = `http://localhost:${debugPort}/json`;
+    const debugCommand = browser === 'Firefox' ? `--start-debugger-server=${debugPort}` : `--remote-debugging-port=${debugPort}`;
     const browserCommand = getBrowserCommand(browser);
+    if(!browserCommand) { return; }
 
     updateStatusBar(browser);
 
@@ -46,12 +48,12 @@ export async function runBrowser(appName: string): Promise<void> {
                 });
                 ws.on('error', (err) => {
                     error(`WebSocket Error: ${err}`);
-                    exec(`${browserCommand} --remote-debugging-port=${targetPort} ${appUrl}`);
+                    exec(`${browserCommand} ${debugCommand} ${appUrl}`);
                     defaultStatusBar();
                 });
             } else {
                 info(`No active sessions, opening a new ${browser} window`);
-                exec(`${browserCommand} --remote-debugging-port=${targetPort} ${appUrl}`);
+                exec(`${browserCommand} ${debugCommand} ${appUrl}`);
                 defaultStatusBar();
                 return;
             }
@@ -79,7 +81,7 @@ export async function runBrowser(appName: string): Promise<void> {
                         if (killErr) {
                             error(`Error closing ${browser}: ${killErr}`);
                         } else {
-                            exec(`${browserCommand} --remote-debugging-port=${targetPort} ${appUrl}`);
+                            exec(`${browserCommand} ${debugCommand} ${appUrl}`);
                         }
                         defaultStatusBar();
                     });
@@ -90,7 +92,7 @@ export async function runBrowser(appName: string): Promise<void> {
             } else {
                 info(`No active sessions, opening a new ${browser} window`);
                 defaultStatusBar();
-                exec(`${browserCommand} --remote-debugging-port=${targetPort} ${appUrl}`);
+                exec(`${browserCommand} ${debugCommand} ${appUrl}`);
             }
         }
     });
